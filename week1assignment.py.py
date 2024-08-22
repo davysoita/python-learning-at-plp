e06471170f5426e3bd4c936ad64fa0d7f7d34709
David Soita <davysoita@gmail.com>
2024-08-22 14:56:34 +0300

    Week 1 assignment

---
+++ week1assignment.py.py
@@ -0,0 +1,6 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+name = input("Enter your name")
+location = input("Enter yor location")
+age = int(input(20))
+print(f'Your name is{name} and your age is {age} and youn come from{location}')
