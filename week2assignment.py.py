5d571f9cb64504dfb4a13ad185cba69bfc4c93af
David Soita <davysoita@gmail.com>
2024-08-25 13:21:38 +0300

    week 2 assignment

---
+++ week2assignment.py.py
@@ -0,0 +1,13 @@
+# creating a list
+my_list = [10, 20, 30, 40]
+# function to add elements to the list using insert method
+my_list.insert(1,15 )
+#creating another list
+another_list = [50, 60, 70]
+#extending my list with another list
+my_list.extend(another_list)
+#function to remove the lasst elemnent from  the list
+my_list.remove(70)
+# sorting the list in ana ascending order
+my_list.sort()
+print(my_list[3])
